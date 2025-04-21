0597e7018370aea3af18a9b52a98e89b3ef65d0b
afydler01 <a.fydler.10@student.scu.edu.au>
2025-04-21 15:14:13 +1000

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,20 +1,81 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { Component, OnInit } from '@angular/core';
+import { InventoryItem } from '../service/my.inventory';
+import { InventoryService } from '../service/inventory.service';
 
-import { Tab3PageRoutingModule } from './tab3-routing.module';
-
-@NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab3PageRoutingModule
-  ],
-  declarations: [Tab3Page]
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: false,
 })
-export class Tab3PageModule {}
+
+export class Tab3Page {
+
+  message:string = ""; // error display message
+  searched_item:string = ""; // stores the searched item name
+
+  item:InventoryItem = new InventoryItem(0, "", "", 0, 0, "", "", 0, "");
+  items:InventoryItem[] = [];
+  displayItems: InventoryItem[] = []; // stores filtered items to display
+  editingItemName: string | null = null; // Name of the item currently being edited
+
+  constructor(private service: InventoryService) {
+    
+  }
+
+//Method copied from 'tab1.page.ts'
+  searchItem() {
+    if (!this.searched_item || this.searched_item.trim().length === 0) {
+      this.message = "Please enter an item name to search.";
+      return;
+    }
+
+    this.service.getItem(this.searched_item).subscribe({
+      next: (item:InventoryItem[]) => {
+        this.item = item[0]
+        this.displayItems = item; // display found item in table
+      },
+      error: (err:any) => {this.message = "Error: " + err.status}
+      });
+  }
+
+
+  // Called when the "Update" button is clicked — changes display to "edit mode"
+  editItem(item: InventoryItem) {
+    this.editingItemName = item.item_name; //Converts plain text to input fields
+  }
+  
+  // Called when the "Cancel" button is clicked during edit — changes display back to "read mode"
+  cancelEdit() {
+    this.editingItemName = null; //Converts input fields of table back to plain text
+  }
+  
+  // confirmUpdate Method  — send updated data of item to the "backend"
+  confirmUpdate(item: InventoryItem) { //Called when the "Confirm" button is clicked
+    this.service.updateItem(item.item_name, item).subscribe({
+      next: (response) => {
+        this.message = `Item '${item.item_name}' updated successfully.`; //If successful, display success message
+        this.editingItemName = null;
+      },
+      error: (err) => { //If error occures during confirmUpdate method, display error message
+        this.message = "Update failed: " + err.status;
+      }
+    });
+  }
+
+  
+  // Delete Method  — Deletes item from database and UI
+  deleteItem(name: string) { // Called when "Delete" button is clicked
+    this.service.deleteItem(name).subscribe({ // Uses the inventory.service.ts to send a DELETE request, as declared in that file, selected item (found by name)
+      next: (res) => {
+        this.message = `Item '${name}' deleted successfully.`; //If successful, display success message
+        this.displayItems = this.displayItems.filter(item => item.item_name !== name);
+      },
+      error: (err) => { //If error occures during delete method, display error message
+        this.message = "Delete failed: " + err.status;
+      }
+    });
+  }
+
+
+}
